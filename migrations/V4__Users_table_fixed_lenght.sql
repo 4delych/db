3a12222230752821f1940b8d8270f64ab093ed93
4delych <adel.bagmanov@mail.ru>
2023-10-19 20:39:54 +0300

    hw4

---
+++ migrations/V4__Users_table_fixed_lenght.sql
@@ -0,0 +1,8 @@
+ALTER TABLE clients
+
+ALTER COLUMN name TYPE varchar(255),
+ALTER COLUMN patronymic TYPE varchar(255),
+ALTER COLUMN profession TYPE varchar(255),
+ALTER COLUMN nationality TYPE varchar(255),
+ALTER COLUMN phone_number TYPE varchar(255),
+ALTER COLUMN email TYPE varchar(255);
